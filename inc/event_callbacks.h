5532ee904b2fb80327f9a9db646c6e5aa81cd629
yoann-heitz <yoann.heitz@polymtl.ca>
2021-04-22 16:01:54 +0200

    Add MPI test and collective functions

--- inc/event_callbacks.h
+++ inc/event_callbacks.h
@@ -34,6 +34,20 @@ OTF2_CallbackCode MpiIsend_callback(OTF2_LocationRef location,
                     uint64_t msgLength,
                     uint64_t requestID);
 
+OTF2_CallbackCode MpiIsendComplete_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID);
+
+OTF2_CallbackCode MpiIrecvRequest_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID);                   
+
 OTF2_CallbackCode MpiRecv_callback(OTF2_LocationRef location,
                     OTF2_TimeStamp time,
                     uint64_t event_position,
@@ -54,5 +68,36 @@ OTF2_CallbackCode MpiIrecv_callback(OTF2_LocationRef location,
                     uint32_t msgTag,
                     uint64_t msgLength,
                     uint64_t requestID);
+
+OTF2_CallbackCode MpiRequestTest(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID);
+
+OTF2_CallbackCode MpiRequestCancelled_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID);
+                
+OTF2_CallbackCode MpiCollectiveBegin_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList);
+
+OTF2_CallbackCode MpiCollectiveEnd_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    OTF2_CollectiveOp collectiveOp,
+                    OTF2_CommRef communicator,
+                    uint32_t root,
+                    uint64_t sizeSent,
+                    uint64_t sizeReceived);                   
 
 void set_events_callbacks(OTF2_EvtReaderCallbacks *event_callbacks);
--- src/event_callbacks.c
+++ src/event_callbacks.c
@@ -65,6 +65,32 @@ OTF2_CallbackCode MpiIsend_callback(OTF2_LocationRef location,
     return OTF2_CALLBACK_SUCCESS;
 }
 
+//Mpi IsendComplete event callback
+OTF2_CallbackCode MpiIsendComplete_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiIsendComplete(user_data->ctx, location, requestID);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
+//Mpi IrecvRequest event callback
+OTF2_CallbackCode MpiIrecvRequest_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiIrecvRequest(user_data->ctx, location, requestID);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
 //Mpi recv event callback
 OTF2_CallbackCode MpiRecv_callback(OTF2_LocationRef location,
                     OTF2_TimeStamp time, uint64_t event_position,
@@ -96,6 +122,61 @@ OTF2_CallbackCode MpiIrecv_callback(OTF2_LocationRef location,
     return OTF2_CALLBACK_SUCCESS;
 }
 
+//Mpi RequestTest event callback
+OTF2_CallbackCode MpiRequestTest_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiRequestTest(user_data->ctx, location, requestID);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
+//Mpi RequestCancelled event callback
+OTF2_CallbackCode MpiRequestCancelled_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    uint64_t requestID){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiRequestCancelled(user_data->ctx, location, requestID);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
+//Mpi CollectiveBegin event callback
+OTF2_CallbackCode MpiCollectiveBegin_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiCollectiveBegin(user_data->ctx, location);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
+//Mpi CollectiveEnd event callback
+OTF2_CallbackCode MpiCollectiveEnd_callback(OTF2_LocationRef location,
+                    OTF2_TimeStamp time,
+                    uint64_t eventPosition,
+                    void *userData,
+                    OTF2_AttributeList *attributeList,
+                    OTF2_CollectiveOp collectiveOp,
+                    OTF2_CommRef communicator,
+                    uint32_t root,
+                    uint64_t sizeSent,
+                    uint64_t sizeReceived){
+    user_data_t *user_data = (user_data_t*)userData;
+    *(user_data->clock_address) = time;
+    barectf_trace_Event_MpiCollectiveEnd(user_data->ctx, location, collectiveOp, communicator, root, sizeSent, sizeReceived);
+    return OTF2_CALLBACK_SUCCESS;
+}
+
 //Function that set all event callbacks to an OTF2 event reader callbacks object
 void set_events_callbacks(OTF2_EvtReaderCallbacks *event_callbacks){
     OTF2_Call(OTF2_EvtReaderCallbacks_SetEnterCallback(event_callbacks,
@@ -105,9 +186,21 @@ void set_events_callbacks(OTF2_EvtReaderCallbacks *event_callbacks){
     OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiSendCallback(event_callbacks,
                                                     &MpiSend_callback));
     OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiIsendCallback(event_callbacks,
-                                                    &MpiIsend_callback));                                                    
+                                                    &MpiIsend_callback));   
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiIsendCompleteCallback(event_callbacks,
+                                                    &MpiIsendComplete_callback));
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiIrecvRequestCallback(event_callbacks,
+                                                    &MpiIrecvRequest_callback));                                                                                                                                                          
     OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiRecvCallback(event_callbacks,
                                                     &MpiRecv_callback)); 
     OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiIrecvCallback(event_callbacks,
-                                                    &MpiIrecv_callback));                                                                                                                                                                                                          
+                                                    &MpiIrecv_callback));
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiRequestTestCallback(event_callbacks,
+                                                    &MpiRequestTest_callback));
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiRequestCancelledCallback(event_callbacks,
+                                                    &MpiRequestCancelled_callback));
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiCollectiveBeginCallback(event_callbacks,
+                                                    &MpiCollectiveBegin_callback));
+    OTF2_Call(OTF2_EvtReaderCallbacks_SetMpiCollectiveEndCallback(event_callbacks,
+                                                    &MpiCollectiveEnd_callback));                                                                                                                                                                                                                                                                                                                                                                                                                          
 }
